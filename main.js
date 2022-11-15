2af89fa84103fc9fd4fde3aa7fede3c6e92ea1af
HunterX-7 <fraquin7@gmail.com>
2022-11-14 19:03:21 -0500

    Fix an issue with the user clicks for the menu to disappear
    Change the styling to make the X button and the menu more similar to figma template
    
    Add JS linters

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 function toggle() {
-    const hamburgerIcon = document.getElementById('hamburger-icon');
-    const closeIcon = document.getElementsByClassName('close-icon')[0];
-    const mobileMenu = document.getElementsByClassName('mobile-menu')[0];
-    hamburgerIcon.classList.toggle('hidden');
-    closeIcon.classList.toggle('hidden');
-    mobileMenu.classList.toggle('hidden');
+  const hamburgerIcon = document.getElementById('hamburger-icon');
+  const closeIcon = document.getElementsByClassName('close-icon')[0];
+  const mobileMenu = document.getElementsByClassName('mobile-menu')[0];
+  hamburgerIcon.classList.toggle('hidden');
+  closeIcon.classList.toggle('hidden');
+  mobileMenu.classList.toggle('hidden');
 }
 
 document.getElementById('hamburger-icon').addEventListener('click', toggle);
-document.getElementsByClassName('close-icon').addEventListener('click', toggle);
-document.getElementsByClassName('mobile-menu').addEventListener('click', toggle);
+document.getElementsByClassName('close-icon')[0].addEventListener('click', toggle);
+document.getElementsByClassName('mobile-menu')[0].addEventListener('click', toggle);
