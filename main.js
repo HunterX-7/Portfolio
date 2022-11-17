bd244e2523b3d2b16edfc1fd1a0a8fd529f99312
HunterX-7 <fraquin7@gmail.com>
2022-11-17 14:50:28 -0500

    Accidentally work locally and made the following changes to the styling for the popup window
    Add the images for the popup window

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* Portfolio - mobile menu */
+
 function toggle() {
   const hamburgerIcon = document.getElementById('hamburger-icon');
   const closeIcon = document.getElementsByClassName('close-icon')[0];
@@ -10,3 +12,4 @@ function toggle() {
 document.getElementById('hamburger-icon').addEventListener('click', toggle);
 document.getElementsByClassName('close-icon')[0].addEventListener('click', toggle);
 document.getElementsByClassName('mobile-menu')[0].addEventListener('click', toggle);
+
