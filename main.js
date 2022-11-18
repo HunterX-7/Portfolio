0a84449d5445590aee24cf9f761203c6e57fc5e5
HunterX-7 <fraquin7@gmail.com>
2022-11-18 12:01:07 -0500

    Fix linter errors

--- main.js
+++ main.js
@@ -13,9 +13,9 @@ document.getElementById('hamburger-icon').addEventListener('click', toggle);
 document.getElementsByClassName('close-icon')[0].addEventListener('click', toggle);
 document.getElementsByClassName('mobile-menu')[0].addEventListener('click', toggle);
 
-//Portfolio: details popup window
+// Portfolio: details popup window
 
-//Obj cards
+// Obj cards
 
 const obj = [{
   name: 'Project Zeus',
@@ -25,71 +25,67 @@ const obj = [{
   live: 'https://hunterx-7.github.io/Portfolio/',
   source: 'https://github.com/HunterX-7/Portfolio',
   projects: ['project1', 'project2', 'project3', 'project4', 'project5', 'project6'],
-}, 
+},
 {
-    name: 'Project Hades',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    image: ['images/SVG/Snapshoot Portfolio (1).svg'],
-    technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
-    live: 'https://hunterx-7.github.io/Portfolio/',
-    source: 'https://github.com/HunterX-7/Portfolio',
-    projects: ['project1', 'project2', 'project3', 'project4', 'project5', 'project6'],
-  }, 
-  {
-    name: 'Project Hera',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    image: ['images/SVG/Snapshoot Portfolio (2).svg'],
-    technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
-    live: 'https://hunterx-7.github.io/Portfolio/',
-    source: 'https://github.com/HunterX-7/Portfolio',
-    projects: ['project1', 'project2', 'project3', 'project4', 'project5', 'project6'],
-  }, 
-  {
-    name: 'Project Athena',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    image: ['images/SVG/Snapshoot Portfolio (3).svg'],
-    technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
-    live: 'https://hunterx-7.github.io/Portfolio/',
-    source: 'https://github.com/HunterX-7/Portfolio',
-    projects: ['project1', 'project2', 'project3', 'project4', 'project5', 'project6'],
-  }, 
-  {
-    name: 'Project Poseidon',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    image: ['images/SVG/Snapshoot Portfolio.svg'],
-    technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
-    live: 'https://hunterx-7.github.io/Portfolio/',
-    source: 'https://github.com/HunterX-7/Portfolio',
-    projects: ['project1', 'project2', 'project3', 'project4', 'project5', 'project6'],
-  }, 
-  {
-    name: 'Project Apollo',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    image: ['images/SVG/Snapshoot Portfolio (1).svg'],
-    technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
-    live: 'https://hunterx-7.github.io/Portfolio/',
-    source: 'https://github.com/HunterX-7/Portfolio',
-    projects: ['project1', 'project2', 'project3', 'project4', 'project5', 'project6'],
-  }]
+  name: 'Project Hades',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  image: ['images/SVG/Snapshoot Portfolio (1).svg'],
+  technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
+  live: 'https://hunterx-7.github.io/Portfolio/',
+  source: 'https://github.com/HunterX-7/Portfolio',
+  projects: ['project1', 'project2', 'project3', 'project4', 'project5', 'project6'],
+},
+{
+  name: 'Project Hera',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  image: ['images/SVG/Snapshoot Portfolio (2).svg'],
+  technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
+  live: 'https://hunterx-7.github.io/Portfolio/',
+  source: 'https://github.com/HunterX-7/Portfolio',
+  projects: ['project1', 'project2', 'project3', 'project4', 'project5', 'project6'],
+},
+{
+  name: 'Project Athena',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  image: ['images/SVG/Snapshoot Portfolio (3).svg'],
+  technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
+  live: 'https://hunterx-7.github.io/Portfolio/',
+  source: 'https://github.com/HunterX-7/Portfolio',
+  projects: ['project1', 'project2', 'project3', 'project4', 'project5', 'project6'],
+},
+{
+  name: 'Project Poseidon',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  image: ['images/SVG/Snapshoot Portfolio.svg'],
+  technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
+  live: 'https://hunterx-7.github.io/Portfolio/',
+  source: 'https://github.com/HunterX-7/Portfolio',
+  projects: ['project1', 'project2', 'project3', 'project4', 'project5', 'project6'],
+},
+{
+  name: 'Project Apollo',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  image: ['images/SVG/Snapshoot Portfolio (1).svg'],
+  technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
+  live: 'https://hunterx-7.github.io/Portfolio/',
+  source: 'https://github.com/HunterX-7/Portfolio',
+  projects: ['project1', 'project2', 'project3', 'project4', 'project5', 'project6'],
+}];
 
-const body = document.querySelector('body');
+// Work-section cards
 
-//Work-section cards
+const workSection = document.getElementById('Portfolio');
+const grid = document.createElement('div');
+grid.classList.add('work-grid');
+workSection.appendChild(grid);
 
- const workSection = document.getElementById('Portfolio');
- const grid = document.createElement('div');
- grid.classList.add('work-grid')
- workSection.appendChild(grid);
- console.log(obj.length)
-
-for (let i=0; i<obj.length; i++){
-
-  const div = document.createElement('div')
-  div.classList.add(i)
+for (let i = 0; i < obj.length; i + 1) {
+  const div = document.createElement('div');
+  div.classList.add(i);
   div.innerHTML = `
                       <div class="container spacing1">
                         <div class="item2 flex-column">
-                            <img class="snapshoot" src="${obj[i].image}" alt="portfolio project image ${i+1}">
+                            <img class="snapshoot" src="${obj[i].image}" alt="portfolio project image ${i + 1}">
                         </div>
                         <div class="item block-column">
                             <h3>${obj[i].name}</h3>
@@ -103,25 +99,25 @@ for (let i=0; i<obj.length; i++){
                         </div>
                       </div>
                     `;
-  grid.appendChild(div)
+  grid.appendChild(div);
 }
 
-//Popup-window cards
+// Popup-window cards
 
-for (let i=0; i<obj.length; i++){
-const xxx = '.popping' + i;
-const open = document.querySelector(xxx);
-open.addEventListener('click', ()=> {
-const main = document.createElement('div');
-main.className = 'main';
-const popup = document.createElement('div');
-popup.className = 'popup';
-popup.innerHTML = `
+for (let i = 0; i < obj.length; i + 1) {
+  const xxx = `.popping${i}`;
+  const open = document.querySelector(xxx);
+  open.addEventListener('click', () => {
+    const main = document.createElement('div');
+    main.className = 'main';
+    const popup = document.createElement('div');
+    popup.className = 'popup';
+    popup.innerHTML = `
                             <div class="work-grid2">
                                 <div class="container3 spacing1">
                                     <div class="item2-pop flex-column">
                                         <button class="btn close" type="button"></button>
-                                        <img class="snapshoot2" src="${obj[i].image}" alt="portfolio project image ${i+1}">
+                                        <img class="snapshoot2" src="${obj[i].image}" alt="portfolio project image ${i + 1}">
                                     </div>
                                     <div class="item-pop block-column">
                                         <div class="wp-div">
@@ -146,12 +142,12 @@ popup.innerHTML = `
                                 </div>
                             </div>
                     `;
-main.appendChild(popup);
-workSection.appendChild(main);
+    main.appendChild(popup);
+    workSection.appendChild(main);
 
-const close = document.querySelector('.close');
-close.addEventListener('click', ()=> {
-    workSection.removeChild(main);
-})
-})
+    const close = document.querySelector('.close');
+    close.addEventListener('click', () => {
+      workSection.removeChild(main);
+    });
+  });
 }
