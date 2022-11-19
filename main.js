bef3d4fea854de217906d224d608492f09b555eb
HunterX-7 <fraquin7@gmail.com>
2022-11-18 20:12:57 -0500

    Add the logic for the form to collect the data and save it in local storage when the page reloads

--- main.js
+++ main.js
@@ -13,15 +13,33 @@ document.getElementsByClassName('mobile-menu')[0].addEventListener('click', togg
 
 document.querySelector('.check').addEventListener('click', (e) => {
   const email = document.getElementById('email').value;
-  const error = document.querySelector('.error');
-  const form = document.getElementsByTagName('form');
-  const lower = email.toLowerCase();
-  if ((email == lower) !== true) {
-    e.preventDefault()
-    error.innerHTML = `<div class="input-fields-error">Your email address needs to be lowercase!</div>`
+  const error = document.querySelector('.error'); const lower = email.toLowerCase();
+  if ((email === lower) !== true) {
+    e.preventDefault();
+    error.innerHTML = '<div class="input-fields-error">Your email address needs to be lowercase!</div>';
   }
 });
 
-const name = document.getElementById('name')
-const email = document.getElementById('email')
-const message = document.getElementById('message')
+// Preserve data in the browser
+
+document.addEventListener('change', () => {
+  const nombre = document.getElementById('name');
+  const email = document.getElementById('email');
+  const message = document.getElementById('message');
+
+  const data = {
+    nombre: nombre.value,
+    email: email.value,
+    message: message.value,
+  };
+  const dataStr = JSON.stringify(data);
+
+  localStorage.setItem('data', dataStr);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const dataUnstr = JSON.parse(localStorage.getItem('data'));
+  document.getElementById('name').value = dataUnstr.nombre;
+  document.getElementById('email').value = dataUnstr.email;
+  document.getElementById('message').value = dataUnstr.message;
+});
